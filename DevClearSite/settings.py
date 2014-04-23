89f70c6120de36300cf18fafafbe3199a3fceccf
Kevin <kyc5cw@virginia.edu>
2014-04-23 13:50:13 -0400

    image fix

--- DevClearSite/settings.py
+++ DevClearSite/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'django-imagekit',
+	'imagekit',
 	'pilkit'
     'DevClear',
     'crispy_forms',
